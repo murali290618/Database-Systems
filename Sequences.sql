6f02e4d4c4706b8837aded3682719102c4897ca9
murali290618 <103545811+murali290618@users.noreply.github.com>
2022-04-12 21:48:07 -0400

    Add files via upload
    Sequences created by me for Project2 of Database Systems(CS 532)

---
+++ Sequences.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Tuesday-April-12-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence LOG_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."LOG_SEQ"  MINVALUE 1000 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1026 NOCACHE  ORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
